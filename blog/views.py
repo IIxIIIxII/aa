7f988c229f9879d88eac0de9424626c13a9b08bc
IIxIIIxII <talipova_kamilla@iuca.kg>
2025-09-30 00:56:52 +0600

    add post_update

--- blog/views.py
+++ blog/views.py
@@ -24,3 +24,17 @@ def post_create(request):
     else:
         form = PostForm()
         return render(request, 'blog/post_form.html', {'form': form})
+    
+
+    
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_form.html', {'form': form})
